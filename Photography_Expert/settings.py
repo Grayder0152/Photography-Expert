a41dbc9b41df5300d2309fda905cc27e839d106b
Sergey Derevianko <derevankosergej0@gmail.com>
2021-02-06 14:08:14 +0200

    add static root

--- Photography_Expert/settings.py
+++ Photography_Expert/settings.py
@@ -80,6 +80,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIR = [STATIC_DIR]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
